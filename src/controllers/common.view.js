071b0c5c7bfbad999b17ef42ea7dbf5a59ac95dd
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2024-01-28 09:21:46 +0100

    Add loginView route and controller

--- src/controllers/common.view.js
+++ src/controllers/common.view.js
@@ -5,6 +5,18 @@ module.exports.signupView = async function (req, res, next) {
     try {
         res.render("signup", {})
     } catch (error) {
-
+        console.log(error)
     }
 }
+
+
+module.exports.loginView = async function (req, res, next) {
+    try {
+        res.render("login", {
+            type: req.params.type
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
--- src/routes/common.view.js
+++ src/routes/common.view.js
@@ -2,5 +2,6 @@ const router = require("express").Router()
 const controllers = require("./../controllers/common.view")
 
 router.get("/", controllers.signupView)
+router.get("/login/:type", controllers.loginView)
 
 module.exports = router
